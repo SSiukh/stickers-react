29b723ac52dc007e13cc66f3c6f9975c3bbf0466
SSiukh <siukhsasha@gmail.com>
2024-09-25 12:09:23 +0300

    test icon

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./Header.module.css";
+import sprite from "./icons.svg";
+
+function Header() {
+  return (
+    <>
+      <header className={styles.header}>
+        <nav className={styles.headerNav}>
+          <a href="#" className={styles.headerLogo}>
+            sticker ua
+          </a>
+          <ul className={styles.headerNavList}>
+            <li className={styles.headerNavItem}>Головна</li>
+            <li className={styles.headerNavItem}>Про нас</li>
+            <li className={styles.headerNavItem}>Каталог</li>
+            <li className={styles.headerNavItem}>Акції</li>
+            <li className={styles.headerNavItem}>Відгуки</li>
+            <li className={styles.headerNavItem}>Замовити</li>
+          </ul>
+        </nav>
+        <div className={styles.headerContacts}>
+          <a href="https://www.instagram.com/_sticker.ua_?igsh=dmtyY20xbXp1YTFo">
+            <svg></svg>
+          </a>
+          <a href="">
+            <svg>
+                <use xlinkHref={`${sprite}#icon-instagram`}></use>
+            </svg>
+          </a>
+          <a href="">
+            <svg></svg>
+          </a>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
