06b44f9facaa6998078aaf34ba46b7bac551eeaa
SSiukh <siukhsasha@gmail.com>
2024-09-25 17:31:07 +0300

    header and hero markup and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,70 @@
 import React from "react";
 import styles from "./Header.module.css";
-import icon from '/telegram.svg';
-import icons from '/icons.svg';
+import icons from "/icons.svg";
 
 function Header() {
   return (
     <>
       <header className={styles.header}>
-        <nav className={styles.headerNav}>
-          <a href="#" className={styles.headerLogo}>
-            sticker ua
-          </a>
-          <ul className={styles.headerNavList}>
-            <li className={styles.headerNavItem}>Головна</li>
-            <li className={styles.headerNavItem}>Про нас</li>
-            <li className={styles.headerNavItem}>Каталог</li>
-            <li className={styles.headerNavItem}>Акції</li>
-            <li className={styles.headerNavItem}>Відгуки</li>
-            <li className={styles.headerNavItem}>Замовити</li>
-          </ul>
-        </nav>
-        <div className={styles.headerContacts}>
-          <a href="https://www.instagram.com/_sticker.ua_?igsh=dmtyY20xbXp1YTFo">
-            <svg><use href={`${icons}#icon-instagram`}></use></svg>
-          </a>
-          <a href="">
-            <svg className={styles.icon}>
-                <use href={`${icon}#telegram`}></use>
-            </svg>
-          </a>
-          <a href="">
-            <svg></svg>
-          </a>
+        <div className="container">
+          <nav className={styles.headerNav}>
+            <a href="#" className={styles.headerLogo}>
+              sticker ua
+            </a>
+            <ul className={styles.headerNavList}>
+              <li className={styles.headerNavLi}>
+                <a href="#" className={styles.headerNavItem}>
+                  Головна
+                </a>
+              </li>
+              <li>
+                <a href="#" className={styles.headerNavItem}>
+                  Про нас
+                </a>
+              </li>
+              <li>
+                <a href="#" className={styles.headerNavItem}>
+                  Каталог
+                </a>
+              </li>
+              <li>
+                <a href="#" className={styles.headerNavItem}>
+                  Акції
+                </a>
+              </li>
+              <li>
+                <a href="#" className={styles.headerNavItem}>
+                  Відгуки
+                </a>
+              </li>
+              <li>
+                <a href="#" className={styles.headerNavItem}>
+                  Замовити
+                </a>
+              </li>
+            </ul>
+
+            <div className={styles.headerContacts}>
+              <a className={styles.headerContactsLink} href="#">
+                <svg width={20} height={20} className={styles.contactIcons}>
+                  <use href={`${icons}#icon-facebook`} />
+                </svg>
+              </a>
+              <a
+                className={styles.headerContactsLink}
+                href="https://www.instagram.com/_sticker.ua_?igsh=dmtyY20xbXp1YTFo"
+              >
+                <svg width={20} height={20} className={styles.contactIcons}>
+                  <use href={`${icons}#icon-instagram`} />
+                </svg>
+              </a>
+              <a className={styles.headerContactsLink} href="">
+                <svg width={16} height={16} className={styles.contactIcons}>
+                  <use href={`${icons}#icon-telegram`} />
+                </svg>
+              </a>
+            </div>
+          </nav>
         </div>
       </header>
     </>
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import logo from "../../assets/hero-logo.png";
+
+function Hero() {
+  return (
+    <section className={styles.hero}>
+      <div className="container">
+        <div className={styles.heroContainer}>
+          <div className={styles.mainContent}>
+            <h1 className={styles.heroTitle}>
+              STICKER UA - магазин якісних наклейок
+            </h1>
+            <p className={styles.heroText}>
+              Перетвори свій байк на витвір мистецтва
+            </p>
+            <button className={styles.heroButton}>Зробити замовлення</button>
+          </div>
+          <img src={logo} alt="logotype" className={styles.heroImg} />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
